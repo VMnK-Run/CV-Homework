573f56ac68f173c458a2b028ce023e058df5c677
VMnK <wang232519@tju.edu.cn>
2023-03-03 01:26:21 +0800

    Assignment1 initial commit

---
+++ Assignment1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <algorithm>
+#include "opencv2/opencv.hpp"
+using namespace std;
+using namespace cv;
+int main() {
+    cout << "hello world" << endl;
+    return 0;
+}
