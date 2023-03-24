76fef2f4583e98ecdddeb38f06a603fe9af99930
VMnK <wang232519@tju.edu.cn>
2023-03-24 23:00:05 +0800

    Assignment3 Initial

---
+++ Assignment3/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv) {
+    cout << "Assignment3 Running!" << endl;
+
+    //TODO Finished this program
+    
+    cout << "Assignment3 Finished!" << endl;
+    return 0;
+}
